44962f58a7d29a5e95bf395c3830f88079b27944
Shikhor Roy <shikhorroy.cse12@gmail.com>
2018-10-19 15:39:53 +0600

    Add name and label constants.

--- src/main/java/test/pack/schemabuilder/constants/Constants.java
+++ src/main/java/test/pack/schemabuilder/constants/Constants.java
@@ -2,6 +2,8 @@ package test.pack.schemabuilder.constants;
 
 public class Constants {
     public static final String FIELDS = "fields";
+    public static final String NAME = "name";
+    public static final String LABEL = "label";
     public static final String FIELD_LIST = "fieldList";
     public static final String OPTIONS = "options";
     public static final String QUERY = "query";
